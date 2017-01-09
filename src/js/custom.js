11e891d11da2a80586eb5fc8a5bb5cb2f03fbc04
jesdiazs <jes.diazserrano@gmail.com>
2017-01-08 23:22:46 -0300

    se crea funcion del primer select

--- src/js/custom.js
+++ src/js/custom.js
@@ -3,14 +3,33 @@ $(document).ready(function(){
 });
 
 // EMPIEZA EL CODIGO DE GOOGLE MAP
-function initMap() {
-  var map = new google.maps.Map(document.getElementById('map'), {
+function initMap(){
+  var map = new google.maps.Map(document.getElementById('map'),{
     zoom: 12,
     center: {lat: -33.45, lng:-70.6667}
   });
 }
 // TERMINA EL CODIGO DE GOOGLE MAP
+// esconder el Selec de vehiculos
+$(".vehi").fadeOut(3000, function(){
+$("botonTwo").fadeOut(1000, function(){
+	}); 
+});  
 
+//EMPIEZA MI FUNCION PARA EL PRIMER SELECT
 
+$("#botonOne").click(function valForm(){
 
-
+	var ciUno = $("#cityOne").val();
+	var ciDos = $("#cityTwo").val();
+	if (ciUno === 0 || ciDos === 0){
+		alert("Debe seleccionar una ciudad de Origen y otra de Destino");
+	}else if(ciUno === ciDos){
+			alert("Deben ser diferente el Origen y el Destino");
+		}else{
+			$("#botonOne").fadeOut(1000, function(){
+			});  
+            $("#cityOne").prop('disabled', 'disabled');
+            $("#cityTwo").prop('disabled', 'disabled');
+        }
+}); 
